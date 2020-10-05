6911c27c56c9e79052156b1512863214834b9f96
yamuna1996d <yamunag19@gmail.com>
2020-10-05 11:57:26 +0530

    product

--- routes/route.js
+++ routes/route.js
@@ -4,6 +4,17 @@ const routers = express.Router();
 routers.get('/', (req, res) => {
     res.send('Hai');
 });
+routers.post('/addpro', async (req, res) => {
+    try {
+        var data = new model(req.body);
+        var result = await data.save();
+        res.json(result);
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
+});
 routers.get('/view', async (req, res) => {
     try {
         var result = await model.find();
